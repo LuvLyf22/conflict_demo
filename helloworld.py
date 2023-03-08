e243970f4da18b420abd90eecd6807aa8a7665dd
Nadine Castillo <nadinecobb22@gmail.com>
2023-03-08 13:32:05 -0800

    corrected merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
-
-
-def main():
-    hello_world("New York")
-=======
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
->>>>>>> 2f128ed02049fcec2d68ebf785441220c5b734b3
-
 
 main()
